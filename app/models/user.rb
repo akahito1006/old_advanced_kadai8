4202a328239dbc327d6e75354090c9c09bcc5de9
akahito1006 <nekodora5k@gmail.com>
2021-03-05 07:46:10 +0000

    [submit] 応用課題7

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :follower, source: :follower_id
-  
   has_many :follow, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
   has_many :follows, through: :follow, source: :follow_id
   
-  
   attachment :profile_image, destroy: false
-
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
   
@@ -30,6 +26,17 @@ class User < ApplicationRecord
     else
     	@user = User.where("name LIKE ?", "%#{word_for_search}%")
     end
+  end
+  
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
   
 end
