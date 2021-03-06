ec05b16d1a26aae4f1e5e34f9f5368d6137269e3
akahito1006 <nekodora5k@gmail.com>
2021-03-06 09:54:32 +0000

    [Update] Mailer building

--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -2,7 +2,7 @@ class ThanksMailer < ApplicationMailer
     
   def send_confirm_to_user(user)
     @user = user
-    mail(subject: "登録が完了しました", to: @user.email) do |format|
+    mail_registration(subject: "登録が完了しました", to: @user.email) do |format|
       format.text
     end
   end
